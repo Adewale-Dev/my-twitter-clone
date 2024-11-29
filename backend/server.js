fbbc28b5bffd9b1cb5c758705be1d6171814885c
Adewale-Dev <iyanurichiee20@gmail.com>
2024-11-29 15:37:27 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({ extended: true }));
+app.get('/', (req,res) => {
+    res.json({data: 'Hello wrld!!!'})
+})
 
 app.use(cookieParser());
 
